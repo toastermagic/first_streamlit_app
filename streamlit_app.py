58db2f32106cdacd166dde9e274c0026a6b43967
easy-going-paul <toastermagic@gmail.com>
2022-11-09 22:24:24 +0000

    kiwi!

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response =  requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response =  requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
