833a9a122b048744c3684bf8ff8c4282572478ea
easy-going-paul <toastermagic@gmail.com>
2022-11-09 23:31:06 +0000

    remove stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,8 +54,6 @@ if streamlit.button("Get Fruit Load List"):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
-
 add_my_fruit = streamlit.text_input('Add a new fruit')
 if streamlit.button('Add a fruit to the list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
